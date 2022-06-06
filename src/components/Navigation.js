a9e3d0c4cf4945300090feb1899c2d3f138bb2b6
kvivek371 <kvivek371@gmail.com>
2022-06-06 12:08:26 +0530

    react assignment 4

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import  '../style.css'
+
+const Navigation = () => {
+  return (
+    <div className='nav__container'>
+      <ul>
+        <li><Link to= "/home" style={{textDecoration: 'none', color : 'aliceblue'}} >HOME</Link> </li>
+        <li> <Link to= "/student" style={{textDecoration: 'none', color : 'aliceblue'} } >Student</Link></li>
+       <li><Link to= "/contact" style={{textDecoration: 'none', color : 'aliceblue'}} >Contact</Link> </li>
+       </ul>
+    </div>
+  )
+}
+
+export default Navigation
